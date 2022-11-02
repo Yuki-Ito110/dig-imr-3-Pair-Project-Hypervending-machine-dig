5b0ceb629d6248f3584d44635d0c52a70a161fa2
YukiIto <yuki_ito_am@mail.toyota.co.jp>
2022-11-02 11:47:50 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+'use strict'
+// 1行目に記載している 'use strict' は削除しないでください
+
+// console.log("hello");
+
+
+// class Vshicle {
+//     startEngine() {
+//         console.log("starting engine...");
+//     }
+// }
+
+// const myVehicle = new Vehicle ();
+// console.log(myVehicle);
+// myVehicle.startEngine();
+
+// class Car extends Vehicle {
+//     constructor(capacity, color, fuelType){
+//         super(); //中で定義されている振る舞いも全て継承すること
+//         this.capacity = capacity;
+//         this.color = color;
+//         this.fuelType = fuelType;
+//     }
+//     break() {
+//         console.log("stop now!");
+//     }
+// }
+
+// const myCar = new Car("1234", 4, "black", "electric");
+// console.log(myCar);
+// myCar.break();
+// myCar.startEngine();
+
+
+
+// //
+// class racingCar extends Car {
+//     constructor(capacity, color, fuelType) {
+
+//     }
+//     drift() {
+//         console.log("drift!");
+//     }
+// }
+
+// const myRacingCar = new racingCar(2, "red", "gasoline")
+
+
