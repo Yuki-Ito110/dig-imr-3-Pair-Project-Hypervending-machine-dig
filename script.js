2ced76707f91593e43f534b25059b632e9354533
YukiIto <yuki_ito_am@mail.toyota.co.jp>
2022-11-02 19:16:16 +0900

    axios set and console.log pokemon URL

--- script.js
+++ script.js
@@ -4,42 +4,37 @@
 
 async function getPokemon () {
     const response = await axios.get("https://pokeapi.co/api/v2/pokemon/25");
-    console.log(response);
+    console.log(response.data.sprites.other["official-artwork"].front_default);
 }
-
 getPokemon();
 
-
 window.addEventListener('load', () => {
+    const containerEl = document.querySelector('#boughtBox');
+    function postImg(image) {
 
+        // 投稿全体のdiv作成
+        const postEl = document.createElement('div');
+        postEl.className = 'drink';
+        
+        // 画像のimage作成
+        const imageEl = document.createElement('img');
+        imageEl.className = "drinks";
+        imageEl.src = image;
 
-
-const containerEl = document.querySelector('#boughtBox');
-
-function postImg(image) {
-    // 投稿全体のdiv作成
-    const postEl = document.createElement('div');
-    postEl.className = 'drink';
-    
-    // 画像のimage作成
-    const imageEl = document.createElement('img');
-    imageEl.className = "drinks";
-    imageEl.src = image;
-
-    // image要素を追加
-    postEl.append(imageEl);
-    
-    containerEl.append(postEl);
+        // image要素を追加
+        postEl.append(imageEl);
+        
+        containerEl.append(postEl);
     }
 
-let postb = document.getElementById('postb');
-postb.addEventListener('click', function() {
-    
-    console.log('postがクリックされました！');
-    postImg("./images/コーラ.jpg");
+    let postb = document.getElementById('postb');
+    postb.addEventListener('click', function() {
+        
+        console.log('postがクリックされました！');
+        postImg("./images/コーラ.jpg");
 
 
-},false);
+    },false);
 
 });
 
