ff5561281392e511f3c13318f4241f10a739546e
YukiIto <yuki_ito_am@mail.toyota.co.jp>
2022-11-02 15:15:14 +0900

    ボタン作成＆飲み物表示まで
    Co-authored-by: Yuki-Ito110 <yuki-ito110@users.noreply.github.com>
    Co-authored-by: hiro1449489 <hiro1449489@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # dig-imr-3-Pair-Project-Hypervending-machine-dig
+
+image or gif
+
+## Overview
+
+## Requirement
+
+## Usage
+
+## Features
+
+## Reference
+
+## Author
+
+[twitter](https://twitter.com/Kotabrog)
+
+## Licence
+
+[MIT](https://......)
--- script.js
+++ script.js
@@ -1,2 +1,34 @@
 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
+
+window.addEventListener('load', () => {
+
+
+const containerEl = document.querySelector('#boughtBox');
+
+function postImg(image) {
+    // 投稿全体のdiv作成
+    const postEl = document.createElement('div');
+    postEl.className = 'drink';
+    
+    // 画像のimage作成
+    const imageEl = document.createElement('img');
+    imageEl.className = "drinks"
+    imageEl.src = image;
+
+    // image要素を追加
+    postEl.append(imageEl);
+    
+    containerEl.append(postEl);
+    }
+
+let postb = document.getElementById('postb');
+postb.addEventListener('click', function() {
+    
+    console.log('postがクリックされました！');
+    postImg("./images/コーラ.jpg");
+
+
+},false);
+
+});
